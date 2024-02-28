740f4e7099e20d009d846ec1e4fc6bc79b45b8a4
zdroz <zdrozal@gmail.com>
2024-02-28 12:06:38 -0500

    Developed calloutReport-view.fxml.

---
+++ src/main/java/com/drozal/dataterminal/reportController.java
@@ -0,0 +1,17 @@
+package com.drozal.dataterminal;
+
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+public class reportController {
+    public Spinner calloutSpinner;
+    public TextField OfficerInfoName;
+    public TextField OfficerInfoRank;
+    public TextField OfficerInfoNumber;
+    public TextField OfficerInfoCallsign;
+    public TextField OfficerInfoAgency;
+    public TextField OfficerInfoDivision;
+
+
+
+}

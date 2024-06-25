7a7301122a8548caf6437066d9c14fe5a0a2f9c3
zdrozlMAC <zdrozal@gmail.com>
2024-06-25 09:55:26 -0400

    - v1.0.5-alpha - Reformat - New Maps, Atlas, Area, Road

--- src/main/java/com/drozal/dataterminal/mapController.java
+++ src/main/java/com/drozal/dataterminal/mapController.java
@@ -1,10 +1,14 @@
 package com.drozal.dataterminal;
 
+import com.drozal.dataterminal.util.Misc.LogUtils;
 import javafx.event.Event;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
+import javafx.scene.layout.VBox;
 
+import static com.drozal.dataterminal.util.Misc.LogUtils.log;
 import static org.controlsfx.tools.Utils.clamp;
 
 public class mapController {
@@ -18,8 +22,17 @@ public class mapController {
 	private double defaultYS;
 	private double defaulutXPos;
 	private double defaulutYPos;
+	private String currentImagePath;
+	@javafx.fxml.FXML
+	private VBox root;
 	
 	public void initialize() {
+		currentImagePath = "/com/drozal/dataterminal/imgs/losSantos.jpg";
+		
+		// Load initial image
+		Image initialImage = new Image(Launcher.class.getResourceAsStream(currentImagePath));
+		losSantosMap.setImage(initialImage);
+		root.setStyle("-fx-background-color:  #143d6b;");
 		defaultTX = losSantosMap.getTranslateX();
 		defaultTY = losSantosMap.getTranslateY();
 		defaultXS = losSantosMap.getScaleX();
@@ -116,4 +129,56 @@ public class mapController {
 		losSantosMap.setX(defaulutXPos);
 		losSantosMap.setY(defaulutYPos);
 	}
+	
+	@javafx.fxml.FXML
+	public void onOverlayPress(Event event) {
+		double defaultTX = losSantosMap.getTranslateX();
+		double defaultTY = losSantosMap.getTranslateY();
+		double defaultXS = losSantosMap.getScaleX();
+		double defaultYS = losSantosMap.getScaleY();
+		double defaulutXPos = losSantosMap.getX();
+		double defaulutYPos = losSantosMap.getY();
+		swapImage();
+		
+		losSantosMap.setTranslateX(defaultTX);
+		losSantosMap.setTranslateY(defaultTY);
+		losSantosMap.setScaleX(defaultXS);
+		losSantosMap.setScaleY(defaultYS);
+		losSantosMap.setX(defaulutXPos);
+		losSantosMap.setY(defaulutYPos);
+		
+	}
+	
+	private void swapImage() {
+		switch (currentImagePath) {
+			case "/com/drozal/dataterminal/imgs/areaMap.jpg":
+				currentImagePath = "/com/drozal/dataterminal/imgs/losSantos.jpg";
+				log("setting lossantos", LogUtils.Severity.DEBUG);
+				root.setStyle("-fx-background-color: #143d6b;");
+				break;
+			case "/com/drozal/dataterminal/imgs/losSantos.jpg":
+				currentImagePath = "/com/drozal/dataterminal/imgs/AtlasMap.jpg";
+				log("setting atlasmap", LogUtils.Severity.DEBUG);
+				root.setStyle("-fx-background-color: #00abd7;");
+				break;
+			case "/com/drozal/dataterminal/imgs/AtlasMap.jpg":
+				currentImagePath = "/com/drozal/dataterminal/imgs/RoadMap.jpg";
+				log("setting roadmap", LogUtils.Severity.DEBUG);
+				root.setStyle("-fx-background-color: #0064b3;");
+				break;
+			case "/com/drozal/dataterminal/imgs/RoadMap.jpg":
+				currentImagePath = "/com/drozal/dataterminal/imgs/areaMap.jpg";
+				log("setting areaMap", LogUtils.Severity.DEBUG);
+				root.setStyle("-fx-background-color: #00c1e1;");
+				break;
+			default:
+				currentImagePath = "/com/drozal/dataterminal/imgs/losSantos.jpg";
+				break;
+		}
+		
+		// Set the new image
+		Image newImage = new Image(Launcher.class.getResourceAsStream(currentImagePath));
+		losSantosMap.setImage(newImage);
+	}
+	
 }
